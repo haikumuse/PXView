2a3653fcad471e1d3aeb5cd3f6579116f9107d89
muse <15767340887@163.com>
2026-06-27 20:31:03 +0800

    加入prompt面板，然后修改路径

--- doc/test_access.cpp
+++
@@ -1,7 +0,0 @@
-#include <QGuiApplication>
-#include <QAccessible>
-int main(int argc, char **argv) { 
-    QGuiApplication app(argc, argv);
-    QAccessible::setActive(false); 
-    return 0; 
-}
--- sync_to_repo.ps1
+++ sync_to_repo.ps1
@@ -9,7 +9,7 @@ if (-Not (Test-Path $TargetDir)) {
     New-Item -ItemType Directory -Force -Path $TargetDir | Out-Null
 }
 
-$FoldersToCopy = @("PXView", "libsigrok", "libsigrokdecode", "common", "lang", "CMake", "debian", "web")
+$FoldersToCopy = @("PXView", "libsigrok", "libsigrokdecode", "common", "lang", "CMake", "debian", "web", "doc")
 $FilesToCopy = @(
     "CMakeLists.txt", "logo-win.ico", "applogo.rc", "README.md", "INSTALL.md", "INSTALL_zh.md", "COPYING", "LICENSE", 
     "PXView.icns", "clean", "cmake_clear", "build_linux", "$([char]0x66F4)$([char]0x65B0)$([char]0x8BF4)$([char]0x660E).txt"
@@ -28,13 +28,7 @@ foreach ($folder in $FoldersToCopy) {
     }
 }
 
-if (Test-Path "$SourceDir\oridoc") {
-    Write-Host "Copying and mapping folder: oridoc -> doc" -ForegroundColor Yellow
-    if (-Not (Test-Path "$TargetDir\doc")) {
-        New-Item -ItemType Directory -Force -Path "$TargetDir\doc" | Out-Null
-    }
-    Copy-Item -Path "$SourceDir\oridoc\*" -Destination "$TargetDir\doc" -Recurse -Force
-}
+
 
 foreach ($file in $FilesToCopy) {
     if (Test-Path "$SourceDir\$file") {
